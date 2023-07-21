ad9fb4ebfd561526342d8266bdf1d92a4904dcf3
Anshu <21bcs5996@cuchd.in>
2023-07-21 12:25:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 (function() {
     'use strict';
 
@@ -9,11 +10,11 @@
     ToBuyController.$inject = ['ShoppingListCheckOffService'];
 
     function ToBuyController(ShoppingListCheckOffService) {
-        var toBuyList = this;
+        var toBuyCtrl = this;
 
-        toBuyList.items = ShoppingListCheckOffService.getToBuyItems();
+        toBuyCtrl.items = ShoppingListCheckOffService.getToBuyItems();
 
-        toBuyList.buyItem = function(itemIndex) {
+        toBuyCtrl.buyItem = function(itemIndex) {
             ShoppingListCheckOffService.buyItem(itemIndex);
         };
     }
@@ -21,31 +22,34 @@
     AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
 
     function AlreadyBoughtController(ShoppingListCheckOffService) {
-        var alreadyBougthList = this;
+        var alreadyBoughtCtrl = this;
 
-        alreadyBougthList.items = ShoppingListCheckOffService.getAlreadyBoughtItems();
+        alreadyBoughtCtrl.items = ShoppingListCheckOffService.getAlreadyBoughtItems();
     }
 
     function ShoppingListCheckOffService() {
         var service = this;
-        var toBuyItems = [
+        var items = [
             { name: "cookies", quantity: 10 },
             { name: "cokes", quantity: 2 },
             { name: "beers", quantity: 6 },
             { name: "apples", quantity: 4 },
-            { name: "bananas", quantity: 7 }
+            { name: "bananas", quantity: 7 },
+            { name: "guava", quantity: 7 }, 
+            { name: "mango", quantity: 7 },
         ];
+
         var alreadyBoughtItems = [];
 
         service.buyItem = function(itemIndex) {
-            var item = toBuyItems[itemIndex];
+            var item = items[itemIndex];
 
             alreadyBoughtItems.push(item);
-            toBuyItems.splice(itemIndex, 1);
+            items.splice(itemIndex, 1);
         };
 
         service.getToBuyItems = function() {
-            return toBuyItems;
+            return items;
         };
 
         service.getAlreadyBoughtItems = function() {
@@ -53,3 +57,4 @@
         };
     }
 })();
+
